4aab1ccbeff4f00a50ec42c8a71d473649dd8544
Tabata Sutili <ssabrinasutili@gmail.com>
2021-04-15 15:13:06 -0300

    teste

---
+++ Details/Classes/DetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailsViewController.swift
+//  Details
+//
+//  Created by Tabata Sabrina Sutili on 15/04/21.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
